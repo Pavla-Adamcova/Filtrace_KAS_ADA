7c65656aff66e59fc6bea3697c52b05e0027873a
Pavlínka Adamcová <pajisek2003@gmail.com>
2022-04-07 14:08:25 +0200

    Znacky, druhy

---
+++ Filtrace_KAS_ADA/Drogerka.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filtrace_KAS_ADA
+{
+
+    class Drogerka
+    {
+        //Konstruktor
+        //int druh, int znacka, int cena, bool dostupnost, string typ, DateTime trvanlivost, int obsah
+    }
+}
