50dfb691da1506866b321be6dd0ed39c6234fa40
Pavla Adamcová <pajisek2003@gmail.com>
2022-04-14 14:40:34 +0200

    Výpis přes podřadné tabulky. Značky fungují, ale ListDruhy se nějakým způsobem mažou a nevypisují se. Nevím, co s tím jako je!

--- Filtrace_KAS_ADA/Drogerka.cs
+++ Filtrace_KAS_ADA/Drogerka.cs
@@ -10,11 +10,25 @@ namespace Filtrace_KAS_ADA
     class Drogerka
     {
         //Vlastnosti
+        public int Druh { get; private set; }
+        public int Znacka { get; private set; }
+        public int Cena {get; private set; }
+        public bool Dostupnost { get; private set; }
+        public string Typ { get; private set; }
+        public DateTime Trvanlivsot { get; private set; }
+        public int Obsah { get; private set; }
 
         //Konstruktor
         public Drogerka(int druh, int znacka, int cena, bool dostupnost, string typ, DateTime trvanlivost, int obsah)
         {
-            
+            //Naplnění
+            Druh = druh;
+            Znacka = znacka;
+            Cena = cena;
+            Dostupnost = dostupnost;
+            Typ = typ;
+            Trvanlivsot = trvanlivost;
+            Obsah = obsah;
         }
         
     }
--- Filtrace_KAS_ADA/Form1.cs
+++ Filtrace_KAS_ADA/Form1.cs
@@ -9,10 +9,12 @@ namespace Filtrace_KAS_ADA
     {
         //deklarace sql
         SqlConnection sqlconnection;
-        SqlCommand sqlCommand;
+        SqlCommand sqlCommandDrogerie;
+        SqlCommand sqlCommandDruhy;
+        SqlCommand sqlCommandZnacky;
 
         //List
-        List<Drogerka> listPolozky;
+        List<Drogerka> listDrogerka = new List<Drogerka>();
 
         //
         int Druh;
@@ -22,58 +24,52 @@ namespace Filtrace_KAS_ADA
         string Typ;
         DateTime Trvanlivost;
         int Obsah;
+        string DruhyString;
+        string ZnackyString;
+        //listy
+        List<string> ListDrogerie;
+        List<string> ListZnacky;
+        List<string> ListDruhy;
         public Form1()
         {
             InitializeComponent();
-            //
-            listPolozky = new List<Drogerka>();
 
             //připojovací řetězec
-            string retezecDB = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=H:\PGV\Database\DB-Drogerie.mdf;Integrated Security=True;Connect Timeout=30";
+            string retezecDB = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\bubel\Documents\Pavlínka\PGV\DB-Drogerie.mdf;Integrated Security=True;Connect Timeout=30";
 
             //připojení
             sqlconnection = new SqlConnection(retezecDB);
 
             // command
-            string command = "SELECT * FROM [Drogerie]";
-            sqlCommand = new SqlCommand(command, sqlconnection);
+            string commandDrogerie = "SELECT * FROM [Drogerie]";
+            string commandDruhy = "SELECT * FROM [Druhy]";
+            string commandZnacky = "SELECT * FROM [Znacky]";
+
+            //sqlCommand
+            sqlCommandDrogerie = new SqlCommand(commandDrogerie, sqlconnection);
+            sqlCommandDruhy = new SqlCommand(commandDruhy, sqlconnection);
+            sqlCommandZnacky = new SqlCommand(commandZnacky, sqlconnection);
+
 
             // otevření
             sqlconnection.Open();
 
+
+
             //práce s daty
-            SqlDataReader zaznamy = sqlCommand.ExecuteReader();
-            while (zaznamy.Read())
-            {
+            SqlDataReader zaznamyDrogerie = sqlCommandDrogerie.ExecuteReader();
+            CteniTabulky(zaznamyDrogerie, 1);
+            zaznamyDrogerie.Close();
 
-                listBDrogerie.Items.Add(($"{zaznamy[1]} - {zaznamy[2]}"));
-                string polozka = "";
-                //do stringu
-                polozka = zaznamy[0].ToString() + ";" + zaznamy[1].ToString() + ";" + zaznamy[2].ToString() + ";" + zaznamy[3].ToString() + ";" + zaznamy[4].ToString() + ";" + zaznamy[5].ToString() + ";" + zaznamy[6].ToString() + ";" + zaznamy[7].ToString();
+            SqlDataReader zaznamyDruhy = sqlCommandDruhy.ExecuteReader();
+            CteniTabulky(zaznamyDruhy, 2);
+            zaznamyDruhy.Close();
 
-                Druh = Convert.ToInt32(zaznamy[1]);
-                Znacka = Convert.ToInt32(zaznamy[2]);
-                Cena = Convert.ToInt32(zaznamy[3]); 
-                Dostupnost = Convert.ToBoolean(zaznamy[4]);
-                Typ = Convert.ToString(zaznamy[5]); 
-                Trvanlivost = Convert.ToDateTime(zaznamy[6]); 
-                Obsah = Convert.ToInt32(zaznamy[7]);
+            SqlDataReader zaznamyZnacky = sqlCommandZnacky.ExecuteReader();
+            CteniTabulky(zaznamyZnacky, 3);
+            zaznamyZnacky.Close();
 
-
-                listPolozky.Add(new Drogerka(Druh, Znacka, Cena, Dostupnost, Typ, Trvanlivost, Obsah));
-               
-            }
-
-            //kontrola pro výpis
-            /*
-            string proInfo = "";
-            for (int i = 0; i < listPolozky.Count; i++) 
-            {
-                proInfo += listPolozky[i].ToString() + Environment.NewLine;
-            }
-            MessageBox.Show(proInfo);
-            */
-
+            Vypis();
 
             //konec připojení
             sqlconnection.Close();
@@ -81,7 +77,7 @@ namespace Filtrace_KAS_ADA
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+        
         }
 
         private void btVytvorit_Click(object sender, EventArgs e)
@@ -90,27 +86,27 @@ namespace Filtrace_KAS_ADA
            // string insertCommand = $@"INSERT INTO [Drogerie] (Id, Druh, Znacka, Cena, Dostupnost, Typ, Trvanlivost, Obsah, Plet, Telo) VALUES ('{tb}');
 
             //otevření databáze
-            sqlconnection.Open();
+            //sqlconnection.Open();
 
             //SqlCommand sqlcommand = new SqlCommand(insertCommand, sqlconnection);
 
             //sqlcommand.ExecuteNonQuery();
 
-            sqlconnection.Close();
+            //sqlconnection.Close();
         }
 
         private void listBDrogerie_SelectedIndexChanged(object sender, EventArgs e)
         {
-            lbInfoCena.Text = lbInfoDostupnost.Text = lbInfoObsah.Text = lbInfoPlet.Text = lbInfoTelo.Text = lbInfoTrvanlivost.Text = lbInfoTyp.Text = lbInfoZnacka.Text = "";
-            int index = listBDrogerie.SelectedIndex;
-            //  string[] pole = listPolozky[index];
+            //lbInfoCena.Text = lbInfoDostupnost.Text = lbInfoObsah.Text = lbInfoPlet.Text = lbInfoTelo.Text = lbInfoTrvanlivost.Text = lbInfoTyp.Text = lbInfoZnacka.Text = "";
+            //int index = listBDrogerie.SelectedIndex;
+            ////  string[] pole = listPolozky[index];
 
-            for (int i = 0; i < listPolozky.Count; i++)
-            {
-                //string[] pole = listPolozky[i].ToString();
+            //for (int i = 0; i < listDrogerka.Count; i++)
+            //{
+            //    //string[] pole = listPolozky[i].ToString();
 
-            }
-            lbInfoZnacka.Text = listPolozky[index].ToString();
+            //}
+            //lbInfoZnacka.Text = listDrogerka[index].ToString();
 
             //lbInfoDruh.Text = pole[1];
             //lbInfoZnacka.Text = pole[2];
@@ -125,6 +121,100 @@ namespace Filtrace_KAS_ADA
         }
         private void radioButtons_CheckedChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void Vypis()
+        {
+            int IDDruhy = 0;
+            int IDZnacka = 0;
+
+            ZnackyString = "";
+            DruhyString = "";
+
+            foreach(Drogerka drogerka in listDrogerka)
+            {
+                IDDruhy = drogerka.Druh;
+                IDZnacka = drogerka.Znacka;
+                MessageBox.Show(Convert.ToString(ListZnacky.Count) + " " + Convert.ToString(ListDruhy.Count));
+                for (int i = 0; i < ListDruhy.Count; i++)
+                {
+                    string dataDruhy = "";
+                    dataDruhy = ListDruhy[i].ToString();
+                    string[] poleDruhy = dataDruhy.Split(';');
+                    if (IDDruhy == Convert.ToInt32(poleDruhy[0]))
+                    {
+                        DruhyString = poleDruhy[1];
+                    }
+                }
+                for (int k = 0; k < ListZnacky.Count; k++)
+                {
+                    string dataZnacky = "";
+                    dataZnacky = ListZnacky[k].ToString();
+                    string[] poleZnacky = dataZnacky.Split(';');
+                    if (IDZnacka == Convert.ToInt32(poleZnacky[0]))
+                    {
+                        ZnackyString = poleZnacky[1];
+                    }
+                }
+                listBDrogerie.Items.Add(DruhyString + " - " + ZnackyString);
+            }
+        }
+
+        private void CteniTabulky(SqlDataReader tabulka, int typ)
+        {
+            ListDrogerie = new List<string>();
+            ListDruhy = new List<string>();
+            ListZnacky = new List<string>();
+            switch (typ)
+            {
+                case 1:
+                    {
+                          while (tabulka.Read())
+                          {
+                                string polozka = "";
+                                //do stringu
+                                polozka = tabulka[0].ToString() + ";" + tabulka[1].ToString() + ";" + tabulka[2].ToString() + ";" + tabulka[3].ToString() + ";" + tabulka[4].ToString() + ";" + tabulka[5].ToString() + ";" + tabulka[6].ToString() + ";" + tabulka[7].ToString();
+                                Druh = Convert.ToInt32(tabulka[1]);
+                                Znacka = Convert.ToInt32(tabulka[2]);
+                                Cena = Convert.ToInt32(tabulka[3]);
+                                Dostupnost = Convert.ToBoolean(tabulka[4]);
+                                Typ = Convert.ToString(tabulka[5]);
+                                Trvanlivost = Convert.ToDateTime(tabulka[6]);
+                                Obsah = Convert.ToInt32(tabulka[7]);
+                                ListDrogerie.Add(polozka);
+                                //do třídy
+                               
+                                Drogerka drogerkaPolozka = new Drogerka(Druh, Znacka, Cena, Dostupnost, Typ, Trvanlivost, Obsah);
+                                listDrogerka.Add(drogerkaPolozka);
+
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        while (tabulka.Read())
+                        {
+                            string polozka = "";
+                            polozka = tabulka[0].ToString() + ";" + tabulka[1].ToString();
+                            ListDruhy.Add(polozka);
+                        }
+                        MessageBox.Show("Druhy " + Convert.ToString(ListDruhy.Count));
+                        break;
+                    }
+                case 3:
+                    {
+                        while (tabulka.Read())
+                        {
+                            string polozka = "";
+                            polozka = tabulka[0].ToString() + ";" + tabulka[1].ToString();
+                            ListZnacky.Add(polozka);
+                        }
+                        MessageBox.Show("Znacky " + Convert.ToString(ListZnacky.Count));
+                        break;
+                    }
+            }
+
 
         }
     }
